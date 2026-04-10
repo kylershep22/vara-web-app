97064b8d408cbb8c0c25b38a9253806543b1fc97
kylershep22 <kylershep@gmail.com>
2026-04-10 14:52:52 -0400

    fix(mobile): strip all console statements from production builds

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -5,7 +5,7 @@ module.exports = function (api) {
     plugins: [
       // Strip console.log/warn/error from production builds
       ...(!process.env.EXPO_PUBLIC_DEBUG && process.env.NODE_ENV === 'production'
-        ? [['transform-remove-console', { exclude: ['error', 'warn'] }]]
+        ? [['transform-remove-console']]
         : []),
     ],
   };
